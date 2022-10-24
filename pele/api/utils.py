e093182f0918355ed57fc3878c46bcb2e89cc180
aslan <baristan1aslan@gmail.com>
2022-10-25 01:16:34 +0200

    Fix bug with available appointments

--- pele/api/utils.py
+++ pele/api/utils.py
@@ -3,13 +3,17 @@ from datetime import datetime
 
 def get_queryset_for_available_appointment(data, appointment_obj):
     current_time = datetime.now().time()
-    current_date = datetime.now().date().strftime('%Y-%m-%d')
-    if current_date == data.get('date'):
-        queryset = appointment_obj.objects.filter(date=data.get('date'),
+    current_date = datetime.now().date()
+    date_db = datetime.strptime(data.get('date'), '%Y-%m-%d').date()
+    if current_date > date_db:
+        return None
+
+    elif current_date == date_db:
+        queryset = appointment_obj.objects.filter(date=date_db,
                                                   time_end__gte=current_time,
                                                   is_available=True)
     else:
-        queryset = appointment_obj.objects.filter(date=data.get('date'),
+        queryset = appointment_obj.objects.filter(date=date_db,
                                                   is_available=True)
 
     return queryset
