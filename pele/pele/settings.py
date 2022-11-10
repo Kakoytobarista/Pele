2287a8fd71eddbfbce6f9db5ce1a7670a8f11186
aslan <baristan1aslan@gmail.com>
2022-11-10 03:18:34 +0100

    Change settings

--- pele/pele/settings.py
+++ pele/pele/settings.py
@@ -59,7 +59,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'https://localhost:8000',
+    'https://pele.myvnc.com',
+    'https://104.248.27.6',
+)
 
 ROOT_URLCONF = 'pele.urls'
 
