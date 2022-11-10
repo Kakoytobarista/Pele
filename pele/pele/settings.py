de101250bb78f76a7d7050a77058edf1e5f98251
aslan <baristan1aslan@gmail.com>
2022-11-10 02:14:46 +0100

    Delete route

--- pele/pele/settings.py
+++ pele/pele/settings.py
@@ -203,7 +203,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3030',
     'http://127.0.0.1:8000',
-    'http://pele.myvnc.com:80/'
+    'http://pele.myvnc.com:80'
 ]
 
 USE_TZ = True
